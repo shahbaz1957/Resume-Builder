40ce7c69753fda8d42d09c2f1382bcd84f14dd55
shahbaz1957 <shahbazjamia1957@gmail.com>
2025-11-03 21:51:44 +0530

    routing setup in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard/>} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder/>} />
+        </Route>
+        <Route path="view/:resumeId" element={<Layout/>} />
+        <Route path="login" element={<Login/>} />
       </Routes>
     </>
   );
