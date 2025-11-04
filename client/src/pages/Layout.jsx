4db6dd49022f2939c499911e6303a47c89a8cb5c
shahbaz1957 <shahbazjamia1957@gmail.com>
2025-11-04 15:55:16 +0530

    add Navbar component

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 function Layout() {
   return (
-    <div>
-      <div>Layout</div>
+    <div className='min-h-screen bg-gray-100'>
+      <Navbar/>
     <Outlet/>
     </div>
   )
